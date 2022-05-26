244a439f9ed5e23c0e33fa2d35ca0b197b5960fc
Kadir Oner <kadironer46500@gmail.com>
2022-05-27 01:03:08 +0300

    Third Commit

--- lib/loginScreen.dart
+++ lib/loginScreen.dart
@@ -19,16 +19,100 @@ class _loginScreenState extends State<loginScreen> {
                 child: Image.asset("images/loginTopShape.png"),
               ),
               Padding(
-                padding: EdgeInsets.only(top:20.0),
+                padding: EdgeInsets.only(top:10.0,bottom:10.0),
                 child: Container(
                   width: MediaQuery.of(context).size.width,
                   child: Center(
                       child: Text("#BİRKULÜPTENDAHAFAZLASI",
                         style: TextStyle(
-                          fontSize: 10,
+                          fontSize: 15,
                           color: Color(0xFF4A4B4D),
+                          fontFamily: "Metropolis",
                       ),
                       ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:  EdgeInsets.only(left: 5.0,right: 5.0,bottom: 10.0),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+
+                    child: Text("“Başarı, zaman içerisinde çok çalışmaya ve tutkuya "
+                        "bağlıdır. Eğer gerçekten çalışırsanız, uzun bir süre"
+                        "boyunca gerçekten çok çalışırsanız, karşılığını "
+                        "alacaksınız.”",
+                      style:TextStyle(
+                        color: Color(0xFF7C7D7E),
+                        fontSize: 12,
+                        fontFamily: "Metropolis",
+                        fontWeight: FontWeight.w500,
+                    ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+
+                ),
+              ),
+              Padding(
+                padding:  EdgeInsets.only(bottom: 15.0),
+                child: Container(
+                  width: 307,
+                  height: 56,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.black,
+                      elevation: 3,
+                      primary: Color(0xFFFBBD14),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(28.0)),
+                      ),
+                    ),
+                    child: Text("Giriş Yap",
+                      style:TextStyle(
+                        color: Color(0xFF3D3D3F),
+                        fontSize: 16,
+                        fontFamily: "Metropolis",
+                        fontWeight: FontWeight.w600,
+                    ),
+                    ),
+                    onPressed: (){
+
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 15.0),
+                child: Container(
+                  width: 307,
+                  height: 56,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.black,
+                      elevation: 3,
+                      primary: Color(0xFFFFFFFF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(28.0)
+                        ),
+                      ),
+                      side: BorderSide(
+                        width: 1.0,
+                        color: Color(0xFFFBBD14),
+                      )
+                    ),
+                    child: Text("Kayıt Ol",
+                      style:TextStyle(
+                        color: Color(0xFF3D3D3F),
+                        fontSize: 16,
+                        fontFamily: "Metropolis",
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    onPressed: (){
+
+                    },
                   ),
                 ),
               )
