4c726b56843d8cfe5d85e2ffcfcac9df0777d06b
Kadir Oner <kadironer46500@gmail.com>
2022-05-27 00:08:26 +0300

    Second commit

--- lib/loginScreen.dart
+++ lib/loginScreen.dart
@@ -11,13 +11,29 @@ class _loginScreenState extends State<loginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-          children: <Widget>[
-            Container(
-              child: Image.asset("images/loginTopShape.png"),
-            )
-
-          ]
+      body: SingleChildScrollView(
+        child: Column(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset("images/loginTopShape.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top:20.0),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+                      child: Text("#BİRKULÜPTENDAHAFAZLASI",
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Color(0xFF4A4B4D),
+                      ),
+                      ),
+                  ),
+                ),
+              )
+            ]
+        ),
       ),
     );
   }
