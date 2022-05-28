bcd1aa925b7a88f812bc16f68d2777a9db29e0a2
Kadir Oner <kadironer46500@gmail.com>
2022-05-28 11:40:46 +0300

    Main Login Screen yapıldı #2

--- lib/mainLoginScreen.dart
+++ lib/mainLoginScreen.dart
@@ -17,7 +17,7 @@ class _mainLoginScreenState extends State<mainLoginScreen> {
             Center(
               child: Container(
                 child: Padding(
-                  padding:  EdgeInsets.only(top: 68.0,bottom: 18.0),
+                  padding:  EdgeInsets.only(top: 50.0,bottom: 18.0),
                   child: Text("Giriş Yap",
                     style:TextStyle(
                       fontSize: 30,
@@ -38,11 +38,14 @@ class _mainLoginScreenState extends State<mainLoginScreen> {
                 ),
               ),
             ),
+
+
+
             Padding(
-              padding: EdgeInsets.only(top: 45.0),
-              child: Container(
-                width: 315,
-                height: 122,
+              padding: EdgeInsets.only(top: 36.0),
+              child: SizedBox(
+                width: 307,
+                height: 56,
                 child: TextField(
                   style: TextStyle(
                     fontSize: 16,
@@ -53,7 +56,6 @@ class _mainLoginScreenState extends State<mainLoginScreen> {
                     hintText: "E-mail",
                     fillColor: Color(0xFFF2F2F2),
                     enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(28.0)),
                       borderSide: BorderSide(
                         width: 1.0,
                         color: Color(0xFF898989),
@@ -68,38 +70,116 @@ class _mainLoginScreenState extends State<mainLoginScreen> {
                     ),
                     ),
                   ),
-                ),
+              ),
             ),
 
-               Container(
-                width: 315,
-                height: 122,
-                child: TextField(
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(20.0),
-                    hintText: "E-mail",
-                    fillColor: Color(0xFFF2F2F2),
-                    enabledBorder: OutlineInputBorder(
+
+
+               Padding(
+                 padding: EdgeInsets.only(top: 28.0),
+                 child: SizedBox(
+                   width: 307,
+                   height: 56,
+                   child: TextField(
+                     style: TextStyle(
+                       fontSize: 16,
+                     ),
+                     keyboardType: TextInputType.visiblePassword,
+                     obscureText: true,
+                     decoration: InputDecoration(
+                       contentPadding: EdgeInsets.all(20.0),
+                       hintText: "Şifre",
+                       fillColor: Color(0xFFF2F2F2),
+                       enabledBorder: OutlineInputBorder(
+
+                         borderSide: BorderSide(
+                           width: 1.0,
+                           color: Color(0xFF898989),
+                         ),
+                       ),
+                       focusedBorder: OutlineInputBorder(
+                           borderRadius: BorderRadius.all(Radius.circular(28.0)),
+                           borderSide: BorderSide(
+                             width: 1.0,
+                             color: Color(0xFF898989),
+                           )
+                       ),
+                     ),
+                   ),
+                 ),
+               ),
+
+
+            Padding(
+              padding: EdgeInsets.only(top: 28.0),
+              child: Container(
+                width: 307,
+                height: 56,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.black,
+                    elevation: 3,
+                    primary: Color(0xFFFBBD14),
+                    shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.all(Radius.circular(28.0)),
-                      borderSide: BorderSide(
-                        width: 1.0,
-                        color: Color(0xFF898989),
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(28.0)),
-                        borderSide: BorderSide(
-                          width: 1.0,
-                          color: Color(0xFF898989),
-                        )
                     ),
                   ),
+                  child: Text("Giriş Yap",
+                    style:TextStyle(
+                      color: Color(0xFF3D3D3F),
+                      fontSize: 16,
+                      fontFamily: "Metropolis",
+                    ),
+                  ),
+                  onPressed: (){
+
+                  },
                 ),
               ),
+            ),
+
+
+            Padding(
+              padding: EdgeInsets.only(top: 10.0,bottom: 10.0),
+              child: Container(
+                child: TextButton(
+                  child: Text("Şifreni mi unuttun ?",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: "Metropolis",
+                      color: Color(0xFF33d3d3f),
+                    ),
+                  ),
+                  onPressed: (){
+                  },
+                ),
+              ),
+            ),
+            
+            
+            Container(
+              width:170,
+              child: Image.asset("images/bottomTransLogo.png"),
+            ),
+
+
+            Padding(
+              padding:  EdgeInsets.only(top: 8.0),
+              child: Container(
+                child: TextButton(
+                  child: Text("Hesabınız yok mu ?"" Kayıt Ol",
+                    style: TextStyle(
+                    fontFamily: "Metropolis",
+                      fontSize: 14,
+                      color: Color(0xFF3d3d3f)
+                  ),
+                  ),
+                  onPressed: (){
+                  },
+                ),
+              ),
+            ),
+
           ],
         ),
       ),
